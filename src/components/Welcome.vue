b4af9f475b4cb867bbe9bf5df98c750a21531372
katharina.sachs <katharina.sachs@seerene.com>
2020-08-14 01:57:50 +0200

    fetch userdata

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -1,7 +1,16 @@
 <template>
-  <b-card bg-variant="dark" text-variant="white" title="Card Title">
+  <b-card bg-variant="light" text-variant="">
     <b-card-text>
-      With supporting text below as a natural lead-in to additional content.
+      Hello: {{ this.current_user.firstname }} !
+    </b-card-text>
+    <b-card-text>
+      Fullname: {{ this.current_user.firstname }} {{ this.current_user.name }}
+    </b-card-text>
+    <b-card-text>
+      Username: {{ this.current_user.username }}
+    </b-card-text>
+    <b-card-text>
+      Kubernetes Service Account: {{ this.current_user.service_account }}
     </b-card-text>
     <b-button href="#" variant="primary">Go somewhere</b-button>
   </b-card>
@@ -14,7 +23,19 @@
 
     components: {
 
+    },
+    data () {
+      return {
+        current_user: {}
+      }
+    },
+    methods: {
+      async fetch_userdata () {
+        this.current_user = await this.$store.dispatch('get_current_user', 's77518')
+      }
+    },
+    created () {
+      this.fetch_userdata()
     }
-
   }
 </script>
--- src/store.js
+++ src/store.js
@@ -1,17 +1,29 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import * as backend from './api/backend'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    showNavigation: false
+    user: {}
   },
 
   getters: {
   },
 
   mutations: {
-    toggleShowNavigation (state) { state.showNavigation = !state.showNavigation }
+    set_user(state, user) { state.user = user}
+
+  },
+  actions: {
+    async get_current_user(context, field) {
+      const user = await backend.readByField('/users', field)
+      console.log('user set ' + user)
+
+      context.commit('set_user', user)
+      console.log('user set ' + user.firstname)
+      return user
+    }
   }
 })
